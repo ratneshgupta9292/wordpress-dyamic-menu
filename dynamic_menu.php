c0278e02ce28172002386b22444d4742cda33d48
ankit <ankit@gmail.com>
2024-12-06 22:09:16 +0530

    first commit

--- dynamic_menu.php
+++ dynamic_menu.php
@@ -1,1 +1,7 @@
-https://stackoverflow.com/questions/22742630/wp-nav-menu-add-class-on-ul
+ https://stackoverflow.com/questions/22742630/wp-nav-menu-add-class-on-ul
+<?php 
+ wp_nav_menu( array(
+    'theme_location' => 'top-menu',
+    'container' => 'ul',
+    'menu_class'=> '[add-your-class-here]'
+ ) );
